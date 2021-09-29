30a10282261a07681ab1013dafaf281004012d9f
Suepaphly <tabenterprise@gmail.com>
2021-09-29 09:49:02 -0500

    Update lisk.js

--- src/services/lisk.js
+++ src/services/lisk.js
@@ -42,11 +42,11 @@ export const fetchSupplyInfo = () => {
 let promises = [];
 let balance = 0;
 let votes = 0;
-for (i = 0; i < 50000; i++) {
+for (let i = 0; i < 50000; i++) {
   promises.push(
     axios.get(node + "/api/v2/accounts?sort=balance:desc&limit=100&offset=" + i).then(res => {
       balance = balance + (res.data.data.summary.balance / 100000000);
-      for(y = 0; y < res.data.data.dpos.sentVotes.length; y++){
+      for(let y = 0; y < res.data.data.dpos.sentVotes.length; y++){
            votes = votes + (res.data.data.dpos.sentVotes[y].amount / 100000000);
       }
     })
