03b756abf09a35140f25557a057ea8e1437615ab
Suepaphly <tabenterprise@gmail.com>
2021-09-29 10:34:39 -0500

    Update lisk.js

--- src/services/lisk.js
+++ src/services/lisk.js
@@ -30,7 +30,7 @@ export const fetchDelegates = (offset) =>
 export const fetchPriceInfo = () =>
   axios
     .get(node + "/api/v2/market/prices")
-    .then(res => res.data.data[3].rate)
+    .then(res => res.data.data[2].rate)
     .catch(err => {
       console.error(err);
       return null;
