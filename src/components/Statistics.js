a4a527845fc02bc975b4006acf71ae6ca8c65739
Suepaphly <tabenterprise@gmail.com>
2021-09-29 13:39:10 -0500

    Update Statistics.js

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -7,20 +7,21 @@ const Statistics = ({ forgers, nodeInfo, price, marketCap, circSupply, totalVolu
       <table className="statsTable">
         <tbody>
           <tr>
-            <td><strong> Height: </strong>{nodeInfo?.height || "N/A"} </td>
-            <td className="textAlignRight">
+            <td><strong> Height: </strong>{nodeInfo?.height || "N/A"} </td>           
+            <td className="textAlignRight"><strong> Price: </strong> $ {Number(price).toFixed(2)} USD (from Kraken) </td>
+          </tr>
+          <tr>
+              <td><strong> Circulating Supply: </strong> {Number(circSupply).toLocaleString("en")} LSK </td>
+              <td className="textAlignRight"><strong> Total Market Cap: </strong> $ {Number(marketCap).toLocaleString("en")} USD </td>
+          </tr>
+          <tr>
+            
+            <td>
              <strong> Next Forgers: </strong> {" "}
               {forgers.map(
                 (forger, i) => `${forger.username}${i !== forgers.length - 1 ? ", " : ""}`
               )}
-            </td>            
-          </tr>
-          <tr>
-              <td><strong> Price: </strong> $ {Number(price).toFixed(2)} USD (from Kraken) </td>
-              <td className="textAlignRight"><strong> Circulating Supply: </strong> {Number(circSupply).toLocaleString("en")} LSK </td>
-          </tr>
-          <tr>
-              <td><strong> Total Market Cap: </strong> $ {Number(marketCap).toLocaleString("en")} USD </td>
+            </td> 
               <td className="textAlignRight"><strong> Volume(24h): </strong> $ {Number(totalVolume).toLocaleString("en")} USD </td>
           </tr>
         </tbody>
