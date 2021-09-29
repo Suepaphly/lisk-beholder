44627b6eab653f517507e3ae8121a4e0b9002fea
Suepaphly <tabenterprise@gmail.com>
2021-09-29 11:56:21 -0500

    Update lisk.js

--- src/services/lisk.js
+++ src/services/lisk.js
@@ -41,27 +41,12 @@ export const fetchPriceInfo = () =>
     });
 
 export const fetchCMCInfo = () => {
-  
-const requestOptions = {
-  method: 'GET',
-  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-  qs: {
-    'start': '1',
-    'limit': '5000',
-    'convert': 'USD'
-  },
-  headers: {
-    'X-CMC_PRO_API_KEY': key
-  },
-  json: true,
-  gzip: true
-};
-
-rp(requestOptions).then(response => {
-  console.log('API call response:', response);
-}).catch((err) => {
-  console.log('API call error:', err.message);
-});
-
+    axios
+    .get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' + key)
+    .then(res => res.data)
+    .catch(err => {
+      console.error(err);
+      return null;
+    });
 };
 
