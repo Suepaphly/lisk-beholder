8dfa2875a9f4969a85f93283d21645fcb63524aa
Suepaphly <tabenterprise@gmail.com>
2022-11-28 18:48:56 -0600

    Update lisk.js

--- src/services/lisk.js
+++ src/services/lisk.js
@@ -1,42 +1,27 @@
-import { node } from "../config/config.json";
+const { apiClient } = require('@liskhq/lisk-client');
 import axios from "axios";
+let clientCache;
 
+export const fetchForgerStats = async () => {
+    const client = await getClient();;
 
+};
 
-export const fetchForgerStats = () =>
-  axios
-    .get(node + "/api/v2/forgers?limit=3")
-    .then(res => res.data.data)
-    .catch(err => {
-      console.error(err);
-      return [];
-    });
+export const fetchNodeInfo = async () => {
+    const client = await getClient();
 
-export const fetchNodeInfo = () =>
-  axios
-    .get(node + "/api/v2/network/status")
-    .then(res => res.data.data)
-    .catch(err => {
-      console.error(err);
-      return null;
-    });
+};
 
-export const fetchDelegates = (offset) =>
-      axios
-        .get(node + "/api/v2/accounts?isDelegate=true&status=active&limit=100&offset=" + offset)
-        .then(res => res.data.data)
-        .catch(err => {
-        console.error(err)
-      });
+export const fetchDelegates = async () => {
+    const client = await getClient();
+    const delegates = await client.block.getByHeight(123)
+    
+};
 
-export const fetchPriceInfo = () =>
-  axios
-    .get(node + "/api/v2/market/prices")
-    .then(res => res.data.data[2].rate)
-    .catch(err => {
-      console.error(err);
-      return null;
-    });
+export const fetchPriceInfo = async () => {
+    const client = await getClient();
+
+};
 
 export const fetchCGInfo = () => 
   axios
