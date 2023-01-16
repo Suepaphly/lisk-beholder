ef49008e7dc54392187a72a4b6d8aed8cefa1e20
ultrafresh <68610613+1ultrafresh@users.noreply.github.com>
2023-01-15 20:55:42 -0600

    Update lisk.js

--- src/services/lisk.js
+++ src/services/lisk.js
@@ -1,4 +1,3 @@
-
 import axios from "axios";
 const { apiClient } = require('@liskhq/lisk-client');
 let clientCache;
@@ -21,24 +20,28 @@ export const fetchForgerStats = async () => {
     const client = await getClient();
     const forgerStats = await client.forger.getStatistics();
     // Do something with forgerStats
+    console.log(forgerStats);
 };
 
 export const fetchNodeInfo = async () => {
     const client = await getClient();
     const nodeInfo = await client.node.getStatus();
     // Do something with nodeInfo
+    console.log(nodeInfo);
 };
 
 export const fetchDelegates = async () => {
     const client = await getClient();
     const delegates = await client.block.getByHeight(123);
     // Do something with delegates
+    console.log(delegates);
 };
 
 export const fetchPriceInfo = async () => {
     const client = await getClient();
     const priceInfo = await client.market.getTicker();
     // Do something with priceInfo
+    console.log(priceInfo);
 };
 
 export const fetchCGInfo = () => 
@@ -50,10 +53,3 @@ export const fetchCGInfo = () =>
       return null;
     });
 
-
-    //This code exports several functions that use the Lisk API client to fetch various types of information from a Lisk node running on the localhost. 
-    //The getClient function creates a WebSocket client for the node if one doesn't already exist and stores it in a cache. 
-    //The useClient function subscribes to an event from the node using the client. 
-    //The fetchForgerStats, fetchNodeInfo, fetchDelegates, and fetchPriceInfo functions use the client to fetch forger statistics, node status, block information by height, and market ticker information respectively. 
-    //The fetchCGInfo function uses the axios library to make a GET request to the CoinGecko API to fetch information about Lisk. 
-    //It returns the data from the API or null if there is an error.
