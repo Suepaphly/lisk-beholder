376358e7f0ade3073c4f064d08f08b6fe08708a6
Suepaphly <tabenterprise@gmail.com>
2023-04-07 04:51:17 -0500

    Update lisk.js

--- src/services/lisk.js
+++ src/services/lisk.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const { apiClient } = require('@liskhq/lisk-client');
-let clientCache;
+let clientCache;    
 
 export const getClient = async () => {
     if (!clientCache) {
@@ -11,8 +11,10 @@ export const getClient = async () => {
 
 export const useClient = async () => {
     const client = await getClient();
-    client.subscribe('pluginAlias:eventAlias', ({ data }) => {
-      console.log(data.info);
+    return new Promise(resolve => {
+        client.subscribe('pluginAlias:eventAlias', ({ data }) => {
+            resolve(data.info);
+        });
     });
 };
 
@@ -20,36 +22,35 @@ export const fetchForgerStats = async () => {
     const client = await getClient();
     const forgerStats = await client.forger.getStatistics();
     // Do something with forgerStats
-    console.log(forgerStats);
+    return forgerStats;
 };
 
 export const fetchNodeInfo = async () => {
     const client = await getClient();
     const nodeInfo = await client.node.getStatus();
     // Do something with nodeInfo
-    console.log(nodeInfo);
+    return nodeInfo;
 };
 
 export const fetchDelegates = async () => {
     const client = await getClient();
     const delegates = await client.block.getByHeight(123);
     // Do something with delegates
-    console.log(delegates);
+    return delegates;
 };
 
 export const fetchPriceInfo = async () => {
     const client = await getClient();
     const priceInfo = await client.market.getTicker();
     // Do something with priceInfo
-    console.log(priceInfo);
+    return priceInfo;
 };
 
 export const fetchCGInfo = () => 
-  axios
-    .get("https://api.coingecko.com/api/v3/coins/lisk?localization=false&community_data=false&developer_data=false")
-    .then(res => res.data)
-    .catch(err => {
-      console.error(err);
-      return null;
-    });
-
+    axios
+        .get("https://api.coingecko.com/api/v3/coins/lisk?localization=false&community_data=false&")
+        .then(res => res.data)
+        .catch(err => {
+            console.error(err);
+            return null;
+        });
