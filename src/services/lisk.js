5d68b178f4d10f73cdd05a225571b300b03d8618
Suepaphly <tabenterprise@gmail.com>
2023-04-08 14:10:36 -0500

    Update lisk.js

--- src/services/lisk.js
+++ src/services/lisk.js
@@ -1,52 +1,49 @@
 import axios from "axios";
-const { apiClient } = require('@liskhq/lisk-client');
-let clientCache;    
 
-export const getClient = async () => {
-    if (!clientCache) {
-        clientCache = await apiClient.createWSClient('ws://localhost:8080/ws');
-    }
-    return clientCache;
-};
+const request = async (method, params) => new Promise(resolve => {
+    const socket = new WebSocket('ws://localhost:9901/ws');
+
+    socket.onopen = () => {
+        socket.send(JSON.stringify({ jsonrpc: '2.0', method, params }));
+    };
+
+    socket.onmessage = (event) => {
+        const answer = JSON.parse(event.data);
+        socket.close();
+        resolve(answer);
+    };
+});
 
 export const useClient = async () => {
-    const client = await getClient();
-    return new Promise(resolve => {
-        client.subscribe('pluginAlias:eventAlias', ({ data }) => {
-            resolve(data.info);
-        });
-    });
+    const data = await request('pluginAlias:eventAlias', {});
+    return data.info;
 };
 
 export const fetchForgerStats = async () => {
-    const client = await getClient();
-    const forgerStats = await client.forger.getStatistics();
+    const forgerStats = await request('get.forgers', {});
     // Do something with forgerStats
     return forgerStats;
 };
 
 export const fetchNodeInfo = async () => {
-    const client = await getClient();
-    const nodeInfo = await client.node.getStatus();
+    const nodeInfo = await request('get.network.status', {});
     // Do something with nodeInfo
     return nodeInfo;
 };
 
 export const fetchDelegates = async () => {
-    const client = await getClient();
-    const delegates = await client.block.getByHeight(123);
+    const delegates = await request('get.blocks', { height: 123 });
     // Do something with delegates
     return delegates;
 };
 
 export const fetchPriceInfo = async () => {
-    const client = await getClient();
-    const priceInfo = await client.market.getTicker();
+    const priceInfo = await request('get.market.prices', {});
     // Do something with priceInfo
     return priceInfo;
 };
 
-export const fetchCGInfo = () => 
+export const fetchCGInfo = () =>
     axios
         .get("https://api.coingecko.com/api/v3/coins/lisk?localization=false&community_data=false&")
         .then(res => res.data)
