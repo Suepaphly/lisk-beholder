882771b32ea9c06f8eb70dbf2b8b245d7c9d6af3
Suepaphly <tabenterprise@gmail.com>
2023-04-13 01:22:56 -0500

    .

--- src/App.js
+++ src/App.js
@@ -29,11 +29,6 @@ const App = () => {
 
   const refreshStats = async () => {
     const forgerStats = await fetchForgerStats();
-    //const nodeInfo = await fetchNodeInfo();
-    //const cgi = await fetchCGInfo();
-    //const marketCap = cgi.market_data.market_cap.usd;
-    //const circSupply = cgi.market_data.circulating_supply;
-    //const totalVolume = cgi.market_data.total_volume.usd;
     
 
     setForgers(forgerStats);
