255804f696b6a18672f2b248b6d3cddd3dcb5d28
Suepaphly <tabenterprise@gmail.com>
2023-04-13 01:32:02 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
 
   const refreshStats = async () => {
     const forgerStats = await fetchForgerStats();
-    
+    console.log("Test");
 
     setForgers(forgerStats);
     setNodeInfo(nodeInfo);
